0725644a153f9a34ae2a937872fcbfd3e5b7a376
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2021-09-05 21:52:07 +0300

    Провую что-то рисовать

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+
+size = widht, height = (960, 540)
+screen = pygame.display.set_mode(size)
+pygame.init()
+
+
+def draw():
+    screen.fill((255, 255, 255))
+    screen.fill(pygame.Color('red'), pygame.Rect(50, 50, 60, 120))
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
