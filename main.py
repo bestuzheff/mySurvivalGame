f65e0adb242165d1afc1d44d3390b52cc609ce5e
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2021-09-06 22:51:29 +0300

    Спрайт и функция загрузки файла

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import pygame
 
 size = widht, height = (960, 540)
 screen = pygame.display.set_mode(size)
+
+def load_image(image_file_name):
+    full_image_file_name = 'data' + '/' + 'images' + '/' + image_file_name
+    try:
+        image = pygame.image.load(full_image_file_name).convert()
+    except any:
+        print("Ошибка загрузка изображения:", image_file_name)
+        raise SystemExit()
+    return image
+
+
+
+
 pygame.init()
 
 
