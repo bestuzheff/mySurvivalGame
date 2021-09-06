1bb3d178795bf5ad356a445bba4c30c993ea9312
Sergej Bestuzhev <sergej.bestuzhev@gmail.com>
2021-09-06 23:17:32 +0300

    Первый выдож картинок на экран

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import pygame
 
-size = widht, height = (960, 540)
+size = widht, height = (1024, 768)
 screen = pygame.display.set_mode(size)
+
 
 def load_image(image_file_name):
     full_image_file_name = 'data' + '/' + 'images' + '/' + image_file_name
     try:
-        image = pygame.image.load(full_image_file_name).convert()
+        image = pygame.image.load(full_image_file_name).convert_alpha()
+        # image = pygame.transform.scale(image, (64, 64)).convert() #Изменение размера картинки
     except any:
         print("Ошибка загрузка изображения:", image_file_name)
         raise SystemExit()
     return image
 
 
+bg = load_image('bg.png')
 
+tree = load_image('tree_9.png')
+for x in range(4):
+    for y in range(3):
+        screen.blit(bg, (x*256, y*256))
+
+screen.blit(tree, (126, 200))
 
 pygame.init()
-
-
-def draw():
-    screen.fill((255, 255, 255))
-    screen.fill(pygame.Color('red'), pygame.Rect(50, 50, 60, 120))
-
-draw()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
